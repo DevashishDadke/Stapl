6b3d3caa1e53b1dda9ae480ead38789705af55ea
Devashish Dadke <dadkedevashish@gmail.com>
2025-11-29 23:53:03 +0530

    resolve activity form state update issue

--- client/src/pages/Activity/LogActivity.jsx
+++ client/src/pages/Activity/LogActivity.jsx
@@ -38,6 +38,7 @@ export default function LogActivity() {
           ← Back
         </button>
 
+
         <h2 className="text-3xl font-bold text-center mb-6 text-gray-800">
           Log New Activity
         </h2>
